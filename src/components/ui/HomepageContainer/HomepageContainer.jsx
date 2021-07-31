41448d50787bd98c0199f24d26bd19f10d8817aa
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-31 22:21:39 +0100

    configure app/adding global store and working on the project structure

---
+++ src/components/ui/HomepageContainer/HomepageContainer.jsx
@@ -0,0 +1,5 @@
+import styles from './HomepageContainer.module.css';
+
+export default function HomepageContainer() {
+  return <section className={styles.homepageContainer}>Box</section>;
+}
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css';
+import { Provider } from 'react-redux';
+import store from '../store';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import '../styles/_globals.css';
+
+function Nextify({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp;
+export default Nextify;
