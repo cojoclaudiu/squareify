f3cb0fed7e1375da72033c0ea73c5f3a4879bc9a
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-31 20:52:14 +0100

    first pages

---
+++ src/pages/browse/index.jsx
@@ -0,0 +1,3 @@
+export default function Browse() {
+  return <div>This is the browse page on this page we will show a map with all the categories</div>;
+}
---
+++ src/pages/browse/technology/index.jsx
@@ -0,0 +1,3 @@
+export default function TechnologyPage() {
+  return <div>Page with tech products</div>;
+}
