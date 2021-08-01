6044be73381629af670d4851cf7b8b185ee944a6
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-08-01 18:06:15 +0100

    Link added for Item component

--- src/components/ui/Item/Item.jsx
+++ src/components/ui/Item/Item.jsx
@@ -1,11 +1,14 @@
+import Link from 'next/link';
 import priceFormat from '../../../../utils/priceFormat';
 import styles from './Item.module.css';
 
-export default function Item({ name, price }) {
+export default function Item({ name, price, url = '/' }) {
   return (
-    <div className={styles.itemContainer}>
-      <h2>{name}</h2>
-      <div className={styles.itemPrice}>{priceFormat(price)}</div>
-    </div>
+    <Link href={url} passHref>
+      <a href="replace" className={styles.itemContainer}>
+        <h2>{name}</h2>
+        <div className={styles.itemPrice}>{priceFormat(price)}</div>
+      </a>
+    </Link>
   );
 }
