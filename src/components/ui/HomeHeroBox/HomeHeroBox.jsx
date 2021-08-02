1cc010972dec9aab7cdae1d99402002cc4ae0aa2
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-08-02 13:49:12 +0100

    mdesigning homepage

--- src/components/ui/HomeHeroBox/HomeHeroBox.jsx
+++ src/components/ui/HomeHeroBox/HomeHeroBox.jsx
@@ -1,12 +1,22 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './HomeHeroBox.module.css';
+
+// Images
+import BackToSchool from '../../../../public/assets/homepage/backtoschool.png';
 
 export default function HomeHeroBox() {
   return (
     <>
       <Link href="/" passHref>
         <a href="replace" className={styles.mainBox}>
-          <div>Mainbox</div>
+          <div className={styles.mainImage}>
+            <Image src={BackToSchool} alt="Back to school Ad" />
+          </div>
+          <div className={styles.mainTitleContainer}>
+            <h2 className={styles.mainTitle}>Are you ready?</h2>
+          </div>
+          <h2 className={styles.mainDescription}>#backtoschool offers</h2>
         </a>
       </Link>
       <Link href="/" passHref>
