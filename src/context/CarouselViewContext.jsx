69eb1013fcaf86ff825d84861f3dcd61fe2ce8f7
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-08-06 00:44:54 +0100

    carousel bug fixed

--- src/context/CarouselViewContext.jsx
+++ src/context/CarouselViewContext.jsx
@@ -1,34 +1,36 @@
 import { createContext, useReducer } from 'react';
 import useWidth from '../hooks/useWidth';
 
-export const CarouselViewContext = createContext({
-  start: null,
-  end: null,
-});
+export const CarouselViewContext = createContext(null);
 
 const carouselViewReducer = (state, action) => {
   switch (action.type) {
     case 'increment':
-      return { start: state.start + 1, end: state.end + 1 };
+      return { start: state.start + 1 };
     case 'decrement':
-      return { start: state.start - 1, end: state.end - 1 };
+      return { start: state.start - 1 };
     default:
       return state;
   }
 };
 
+const widths = [621, 921, 1240];
+
+const getNumItems = (windowWidth) => {
+  const index = widths.findIndex((width) => windowWidth < width);
+  return index === -1 ? 4 : index + 1;
+};
+
 const CarouselViewProvider = ({ children }) => {
   const width = useWidth();
 
-  function getNumItems(windowWidth) {
-    if (windowWidth < 621) return 1;
-    if (windowWidth < 921) return 2;
-    if (windowWidth < 1240) return 3;
-    return 4;
-  }
-
   const [state, dispatch] = useReducer(carouselViewReducer, { start: 0 });
-  const values = { start: state.start, end: state.start + getNumItems(width) };
+  const numItems = getNumItems(width);
+  const diff = state.start - numItems;
+  const values = {
+    start: diff < 0 ? state.start : diff,
+    end: diff < 0 ? state.start + numItems : state.start,
+  };
   const value = { state: values, dispatch };
   return <CarouselViewContext.Provider value={value}>{children}</CarouselViewContext.Provider>;
 };
