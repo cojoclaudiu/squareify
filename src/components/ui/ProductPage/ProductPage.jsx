c3e746bf0774652b8a255f0afac6c710b8a17431
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-08-06 11:02:42 +0100

    Product page structure

--- src/components/ui/ProductPage/ProductPage.jsx
+++ src/components/ui/ProductPage/ProductPage.jsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import useData from '../../../hooks/useData';
+import priceFormat from '../../../utils/priceFormat';
 import styles from './ProductPage.module.css';
 
 export default function ItemPage() {
@@ -9,16 +11,35 @@ export default function ItemPage() {
   const itemData = items.find((item) => item.id === id);
 
   return (
-    <div className={styles.itemPageContainer}>
+    <main className={styles.itemContainer}>
       {itemData === undefined ? (
         'loading'
       ) : (
         <>
-          <h1>{itemData.name}</h1>
-          <div>{itemData.price}</div>
-          <div>{itemData.description}</div>
+          <article className={styles.productMain}>
+            <h1 className={styles.productTitle}>{itemData.name}</h1>
+            <div className={styles.productGallery}>
+              <div className={styles.mainImage}>
+                <Image width={400} height={400} src={itemData.image} />
+              </div>
+              <div className={styles.secondaryImage}>
+                <Image width={400} height={400} src={itemData.image} />
+              </div>
+              <div className={styles.thumbContainer}>
+                <div className={styles.thumbImage}>Item </div>
+                <div className={styles.thumbImage}>Item </div>
+                <div className={styles.thumbImage}>Item </div>
+                <div className={styles.thumbImage}>Item </div>
+              </div>
+            </div>
+            <p className={styles.productDescription}>{itemData.description}</p>
+          </article>
+
+          <section className={styles.productSidebar}>
+            <h2 className={styles.productPrice}>{priceFormat(itemData.price)}</h2>
+          </section>
         </>
       )}
-    </div>
+    </main>
   );
 }
